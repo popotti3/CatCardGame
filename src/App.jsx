0e6e3211b9baba494f18776c0344e5dc6545ecf8
popotti2 <aapo1anttila@gmail.com>
2023-10-06 18:18:42 +0300

    testi

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,71 @@
 import Card from "./Components/Card";
 import './App.css'
+import { useState } from "react";
 
+const getRandomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 const playerCard ={
-  image: 'http://placekitten.com/120/100',
-  stats:[{name:'Cutenes かわいさ', value: 9},{ name:'Weight 重さ',value: 20},{name:'Speed スピード', value: 200}]
-  //stats2:[{name:'Weight 重さ', value: 20}]
+  image: 'http://placekitten.com/120/100?image='+getRandomInt(0,15),
+  stats:[{name:'Cutenes かわいさ', value: getRandomInt(1,100)},{ name:'Weight 重さ',value:getRandomInt(1,100)},{name:'Speed スピード', value: getRandomInt(1,100)}]
+ 
 };
 const enemyCard ={
-  image: 'http://placekitten.com/120/100?image=2',
-  stats:[{name:'Cutenes かわいさ', value: 23},{ name:'Weight 重さ',value: 4},{name:'Speed スピード', value: 16}]
+  image: 'http://placekitten.com/120/100?image='+getRandomInt(0,15),
+  stats:[{name:'Cutenes かわいさ', value: getRandomInt(1,100)},{ name:'Weight 重さ',value: getRandomInt(1,100)},{name:'Speed スピード', value: getRandomInt(1,100)}]
+  
 };
+
+const createCaed = index =>({
+  image: 'http://placekitten.com/120/100?image='+ index,
+  stats:[{name:'Cutenes かわいさ', value: getRandomInt(1,100)},
+          {name:'Weight 重さ',value:getRandomInt(1,100)},
+          {name:'Speed スピード', value: getRandomInt(1,100)}],
+ 
+  id:crypto.randomUUID()
+})
 
 
 export default function app(){
-  return(
-  <div>
-    
-    <h1>Hello world</h1>
-    <Card card = {playerCard}/>
-    <Card card = {enemyCard}/>
+  const [result, setResult] = useState('');
+  function compareCards(){
+
+    const playerStat = playerCard.stats[0];
+    const opponentStat = enemyCard.stats[0];
+
    
-  </div>
- 
+
+    if (playerStat.value === opponentStat.value){
+      setResult('Draw')
+    }
+    else if (playerStat.value > opponentStat.value){
+      setResult('Win')
+    }
+    else{
+      setResult('Loss')
+    }
+
+    console.log(result)
+  }
+
+
+
+  return(
+    <>
+    <h1>Hello world</h1>
+    <div className='game'>
+    
+      
+      <Card card = {playerCard}/>
+      <div className="center-area">
+        <p>{result || 'Press the button'}</p>
+            <button onClick={compareCards}type="button">Play</button>
+      </div>
+   
+      
+      
+      <Card card = {enemyCard}/>
+   
+    </div>
+  </>
   );
 }
